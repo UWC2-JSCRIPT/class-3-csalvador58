0707c9e98b21c22ab45c963911dbc13e6d8cd67b
Salvador <csalvador58@gmail.com>
2022-11-02 10:58:57 -0700

    Spaceship problem completed

--- spaceShip.js
+++ spaceShip.js
@@ -9,12 +9,19 @@ class SpaceShip {
         this.topSpeed = topSpeed;
     }
     accelerate() {
-        console.log(`${this.name} moving to ${this.topSpeed}`)
+        console.log(`${this.name} moving to ${this.topSpeed} light-years per second.`)
     }
 }
 
 // 2. Call the constructor with a couple ships, 
 // and call accelerate on both of them.
 
-const myShip = new SpaceShip('myShip', 100);
-console.log(myShip.accelerate())
+const gaurdiansGalaxy = new SpaceShip('The Milano', 183);
+gaurdiansGalaxy.accelerate();
+// The Milano moving to 183 light-years per second.
+const transformersAutobots = new SpaceShip('The Ark', 115);
+transformersAutobots.accelerate();
+// The Ark moving to 115 light-years per second.
+const starWars = new SpaceShip('Millennium Falcon', 0.75);
+starWars.accelerate();
+// Millennium Falcon moving to 0.75 light-years per second.
